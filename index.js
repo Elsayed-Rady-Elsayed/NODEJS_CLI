71623c1d2fbc855571193c03b3b94cda10c0e5fe
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-19 12:36:21 +0200

    cli with nodejs

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ program
 program
   .command("add")
   .alias("a")
-  .description("add a course")
+  .description("add a project")
   .argument("<title>", "add project title")
   .option("--price <price>", "add project price")
   .action((param, opt) => {
     console.log(param);
     console.log(opt);
+  });
+program
+  .command("list")
+  .alias("l")
+  .description("list all projects")
+  .action(() => {
+    console.log("====================================");
+    console.log("all projects");
+    console.log("====================================");
   });
 program.parse(process.argv);
